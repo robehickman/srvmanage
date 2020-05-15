99a9fff5770732e6fccd418d28466b5fd03a4f2a
Robert Hickman <you@example.com>
2020-05-15 14:11:34 +0100

    update backup script

--- client_scripts/scripts/backup_server.py
+++ client_scripts/scripts/backup_server.py
@@ -2,38 +2,41 @@
 import os, fcntl, sys
 from pathlib import Path
 
-path = Path(sys.argv[1])
-account = 'root@robehickman.com'
+path    = Path(sys.argv[1])
+account = Path(sys.argv[2])
 
 os.system("cd " + str(path))
 
-#Lock to prevent two simultaneous sync attempts, as we are calling with chron
+#Lock to prevent two simultaneous sync attempts, as we are calling with cron
 lockfile = open(str(path / 'lock'), 'w')
-fcntl.flock(fd, LOCK_EX | LOCK_NB)
+fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
 
 # Run the backup
 print("-- Backup Mysql --")
-os.system(f"ssh {account} 'python /root/sql_backup.py'")
-os.system(f"rsync -a {account}:/root/db_backup/ ./db_backup --delete")
+os.system(f"ssh {account} backup_mysql.py")
+os.system(f"mkdir -p {path / 'db_backup'}")
+os.system(f"rsync -a {account}:/root/mysql_backup/ {path / 'db_backup' / 'mysql'} --delete")
 
 print("-- Backup Apache --")
-os.system(f"rsync -a {account}:/etc/apache2/ ./apache_conf --delete")
+os.system(f"rsync -a {account}:/etc/apache2/ {path / 'apache_conf'} --delete")
 
 print("-- Backup PHP --")
-os.system(f"rsync -a {account}:/etc/php/ ./php_conf --delete")
+os.system(f"rsync -a {account}:/etc/php/ {path / 'php_conf'} --delete")
 
 print("-- Backup HTTP --")
-os.system(f"rsync -a {account}:/srv/http/ --exclude=*.git* ./http --delete")
+os.system(f"rsync -a {account}:/srv/http/ --exclude=*.git* {path / 'http'} --delete --exclude=.git*")
 
 print("-- Committing changes to git --")
-os.system("cd db_backup")
-os.system("git add .")
-os.system("git commit -m 'backup'")
-os.system("git gc")
-os.system("cd " + str(path))
 
-os.system("date > last_run.txt")
+if not os. path. isdir(path / 'db_backup' / '.git'):
+    os.system(f"git -C {path / 'db_backup'} init")
+
+os.system(f"git -C {path / 'db_backup'} add .")
+os.system(f"git -C {path / 'db_backup'} commit -m 'backup'")
+os.system(f"git -C {path / 'db_backup'} gc")
+
+os.system(f"date > {path / 'last_run.txt'}")
 
 # Unlock
-fcntl.flock(fd, LOCK_UN)
+fcntl.flock(lockfile, fcntl.LOCK_UN)
 
