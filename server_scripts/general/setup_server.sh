0baca7d37e080f0d621d4d43196e3428c97318d3
Robert Hickman <robehickman@gmail.com>
2025-06-21 15:02:33 +0100

    update setup script to install more things automatically

--- server_scripts/general/setup_server.sh
+++ server_scripts/general/setup_server.sh
@@ -1,15 +1,25 @@
 apt-get update
 apt-get upgrade
 
-apt-get install -y htop git
 
-# Install mysql
+# --------------------------------------------------------------------------
+# Install system utils and tools needed to install other tools
+# --------------------------------------------------------------------------
+apt-get install -y htop git wget curl
+
+
+# --------------------------------------------------------------------------
+# Install mariadb
+# --------------------------------------------------------------------------
 apt-get install -y mariadb-server
 service mysql start
 
+
+# --------------------------------------------------------------------------
 # Install and setup apache2
+# --------------------------------------------------------------------------
 apt-get update
-apt-get install apache2
+apt-get install -y apache2
 
 a2enmod http2
 a2enmod rewrite
@@ -27,8 +37,11 @@ EOF
 )
 echo "$ACMECONF" > /etc/apache2/conf-enabled/zz_md_settings.conf
 
+
+# --------------------------------------------------------------------------
 # Install and setup PHP
-apt install apt-transport-https lsb-release ca-certificates wget
+# --------------------------------------------------------------------------
+apt install -y apt-transport-https lsb-release ca-certificates wget
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list' 
 apt update 
@@ -36,24 +49,77 @@ apt update
 apt-get install php8.3-fpm php8.3-{bcmath,bz2,intl,curl,gd,mbstring,mysqli,zip,imagick}
 phpenmod mbstring
 
+
+# --------------------------------------------------------------------------
 # Install nodejs and rollup
-curl -sL https://deb.nodesource.com/setup_10.x -o /tmp/nodesource_setup.sh
+# --------------------------------------------------------------------------
+curl -fsSL https://deb.nodesource.com/setup_23.x -o /tmp/nodesource_setup.sh
 bash /tmp/nodesource_setup.sh
-apt install nodejs
+apt install -y nodejs
 npm install -g rollup
 
-# Install a few python packages
-apt-get install -y python3-pip
-pip3 install pymysql
-pip3 install pyyaml
 
+# --------------------------------------------------------------------------
+# Install cli imagemagick
+# --------------------------------------------------------------------------
+apt install imagemagick
+
+
+# --------------------------------------------------------------------------
+# Install cli abc notation converter
+# --------------------------------------------------------------------------
+apt install abcm2ps
+
+
+# --------------------------------------------------------------------------
+# Install lilypond
+# --------------------------------------------------------------------------
+wget https://gitlab.com/lilypond/lilypond/-/releases/v2.24.4/downloads/lilypond-2.24.4-linux-x86_64.tar.gz
+
+tar -xvf lilypond-2.24.4-linux-x86_64.tar.gz 
+
+mv -a lilypond-2.24.4 /opt
+
+ln -s /opt/lilypond-2.24.4/bin/lilypond /usr/bin/lilypond
+ln -s /opt/lilypond-2.24.4/bin/convert-ly /usr/bin/convert-ly
+
+chown -R root:root /opt/lilypond-2.24.4
+chown -R root:root /usr/bin/lilypond
+chown -R root:root /usr/bin/convert-ly
+
+chmod -R 755 /opt/lilypond-2.24.4
+chmod -R 755 /usr/bin/lilypond
+chmod -R 755 /usr/bin/convert-ly 
+
+
+# --------------------------------------------------------------------------
+# Install a few python packages
+# --------------------------------------------------------------------------
+apt inatall -y python3-pymysql
+apt inatall -y python3-yaml
+
+
+# --------------------------------------------------------------------------
+# Install server management system
+# --------------------------------------------------------------------------
+git clone https://github.com/robehickman/srvmanage.git
+cd srvmanage/server_scripts 
+pip3 install --break-system-packages . 
+
+
+# --------------------------------------------------------------------------
 # Create directories
+# --------------------------------------------------------------------------
 mkdir /srv/http
 mkdir /srv/repos
 mkdir /srv/repos/web
 mkdir /root/ssh_keys
 
-# Setup directories and links for backup system
+
+# --------------------------------------------------------------------------
+# Setup directories and links for backup system (rrbackup,
+# not installed by this setup script)
+# --------------------------------------------------------------------------
 mkdir /root/remote_backup
 mkdir /root/remote_backup/backup
 mkdir /root/remote_backup/backup/mysql_backup
@@ -61,4 +127,5 @@ ln -s /etc/apache2 /root/remote_backup/backup/apache2
 ln -s /etc/php /root/remote_backup/backup/php
 ln -s /srv/http /root/remote_backup/backup/http
 ln -s /srv/repos /root/remote_backup/backup/repos
+
 
